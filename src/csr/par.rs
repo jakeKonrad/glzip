0998c87496e372416b8cc64872e553fc8a8b9edd
Jacob Griffith Konrad <jakegkonrad@gmail.com>
2022-03-25 11:09:00 +0000

    Parallelized recursion in prob_calc

--- src/csr/par.rs
+++ src/csr/par.rs
@@ -182,9 +182,11 @@ fn calc_prop(v: u32, mut sizes: slice::Iter<'_, usize>, graph: &CSR, p: &[CacheP
             }
         }
 
-        for u in graph.adj(v) {
-            calc_prop(u, sizes.clone(), graph, p);
-        }
+        rayon::scope(|s| {
+            for u in graph.adj(v) {
+                s.spawn(|| calc_prop(u, sizes.clone(), graph, p));
+            }
+        });
     }
 }
 
