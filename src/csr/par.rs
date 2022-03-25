79b47a8985acf7041268ba2be804eb0fac602150
Jacob Griffith Konrad <jakegkonrad@gmail.com>
2022-03-25 11:25:10 +0000

    The prev commit was slower

--- src/csr/par.rs
+++ src/csr/par.rs
@@ -182,12 +182,9 @@ fn calc_prop(v: u32, mut sizes: slice::Iter<'_, usize>, graph: &CSR, p: &[CacheP
             }
         }
 
-        rayon::scope(|s| {
-            for u in graph.adj(v) {
-                let new_sizes = sizes.clone();
-                s.spawn(move |_| calc_prop(u, new_sizes.clone(), graph, p));
-            }
-        });
+        for u in graph.adj(v) {
+            calc_prop(u, sizes.clone(), graph, p);
+        }
     }
 }
 
